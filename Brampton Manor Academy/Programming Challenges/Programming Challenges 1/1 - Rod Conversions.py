8ad3b729f052c34c70cc45a6de18a1686a8a10b5
Sadat154 <113169631+Sadat154@users.noreply.github.com>
2022-09-12 20:16:35 +0100

    Update 1 - Rod Conversions.py

--- Brampton Manor Academy/Programming Challenges/Programming Challenges 1/1 - Rod Conversions.py
+++ Brampton Manor Academy/Programming Challenges/Programming Challenges 1/1 - Rod Conversions.py
@@ -1,38 +1,38 @@
 rods = float(input("Input Rods: "))
 
 
-def rTm(rods):
+def rods_to_meters(rods):
     rodsToMeters = rods * 5.0292
     return rodsToMeters
 
 
-def rTf(rodsToMeters):
+def rods_to_feet(rodsToMeters):
     rodsToFeet = rodsToMeters / 0.3048
     return rodsToFeet
 
 
-def rTmi(rodsToMeters):
+def rods_to_miles(rodsToMeters):
     rodsToMiles = rodsToMeters / 1609.34
     return rodsToMiles
 
 
-def rTfu(rods):
+def rods_to_furlongs(rods):
     rodsToFurlongs = rods / 40
     return rodsToFurlongs
 
 
-def minutes(rodsToMiles):
+def minutes_calculation(rodsToMiles):
     rodMinutes = (rodsToMiles / 3.1) * 60
     return rodMinutes
 
 
 # print("Meters:",rTm(rods),"Feet:", rTf(rTm(rods)), "Miles:",rTmi(rTm(rods)),"Furlongs:", rTfu(rods),"Minutes To walk", rods, "rods:", minutes(rTmi(rTm(rods))))
 def run():
-    print(f"Meters: {rTm(rods)}")
-    print(f"Feet: {rTf(rTm(rods))}")
-    print(f"Miles: {rTmi(rTm(rods))}")
-    print(f"Furlongs: {rTfu(rods)}")
-    print(f"Minutes to walk {rods}: {minutes(rTmi(rTm(rods)))}")
+    print(f"Meters: {rods_to_meters(rods)}")
+    print(f"Feet: {rods_to_feet(rods_to_meters(rods))}")
+    print(f"Miles: {rods_to_miles(rods_to_meters(rods))}")
+    print(f"Furlongs: {rods_to_furlongs(rods)}")
+    print(f"Minutes to walk {rods}: {minutes_calculation(rods_to_miles(rods_to_meters(rods)))}")
 
 
 if __name__ == "__main__":
