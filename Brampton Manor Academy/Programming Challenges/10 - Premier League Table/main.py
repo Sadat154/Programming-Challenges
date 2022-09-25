7d361b2f5cf0ffa13a55f8b6c9d9b8aa7dee4986
Sadat154 <113169631+Sadat154@users.noreply.github.com>
2022-09-25 11:46:01 +0100

    Add files via upload

---
+++ Brampton Manor Academy/Programming Challenges/10 - Premier League Table/main.py
@@ -0,0 +1,84 @@
+import csv
+from pathlib import Path
+
+csv_file = Path("Premier 16-17 - Premier 16-17.csv")
+
+
+def check_file_exists(csv_file):
+    return csv_file.is_file()
+
+
+def read_csv(csv_file):
+    csv_contents = []
+    if check_file_exists(csv_file):
+        with open(csv_file) as csvfile:
+            reader = csv.reader(csvfile, delimiter=",")
+            next(reader)  ###   skip first row (header)
+            for row in reader:
+                csv_contents.append(row)
+    return csv_contents
+
+
+def process_results(rows):
+    originalVal = rows
+    team_name_dictionary = {}
+    counter = 0
+
+    for row in rows:
+        teams = originalVal[counter]
+
+        team_name_dictionary[teams[1]] = [0,0,0,0,0,0,0] # 5 = goals scored, 6 = goals conceded,3 = goal difference
+
+        counter += 1
+
+
+    counter = 0
+
+    for row in rows:
+        teams = originalVal[counter]
+
+        if teams[5] == "H": #4 = total points,3 = goal dif, 2 = losses, 1 = draws, 0 = wins
+            team_name_dictionary[teams[1]][4] += 3
+            team_name_dictionary[teams[1]][0] += 1
+            team_name_dictionary[teams[2]][2] += 1
+
+
+
+
+        elif teams[5] == "A":
+            team_name_dictionary[teams[2]][4] += 3
+            team_name_dictionary[teams[2]][0] += 1
+            team_name_dictionary[teams[1]][2] += 1
+
+
+
+
+        else:
+            team_name_dictionary[teams[1]][4] += 1
+            team_name_dictionary[teams[1]][1] += 1
+
+
+            team_name_dictionary[teams[2]][4] += 1
+            team_name_dictionary[teams[2]][1] += 1
+
+        team_name_dictionary[teams[1]][5] += (int(teams[3]))
+        team_name_dictionary[teams[2]][5] += (int(teams[4]))
+
+        team_name_dictionary[teams[1]][6] += int(teams[4])
+        team_name_dictionary[teams[2]][6] += int(teams[3])
+
+        counter +=1
+
+    listedTeamNames = sorted(team_name_dictionary.items(), key=lambda x: x[1], reverse=True)
+    return(listedTeamNames)
+
+
+print(f"{'Place':<10} {'Team Name':<20} {'Wins':<10} {'Draws':<10} {'Losses':<10} {'Goal Difference':<20} {'Total Points':<10}")
+
+if __name__ == "__main__":
+    file_contents = read_csv(csv_file)
+    teamName = process_results(file_contents)
+
+
+    for key in teamName:
+        print(f"{key[0]:>20}{key[1][0]:>15}{key[1][1]:>11}{key[1][2]:>12}{(key[1][5]-key[1][6]):>15}{key[1][4]:>20}")
