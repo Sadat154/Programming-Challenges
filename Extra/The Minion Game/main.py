0d7f2d1009a258dd7be393f0ac76bfb65d04dc73
Sadat154 <113169631+Sadat154@users.noreply.github.com>
2022-10-21 09:38:03 +0100

    Update main.py

--- Extra/The Minion Game/main.py
+++ Extra/The Minion Game/main.py
@@ -13,32 +13,15 @@ def dictOfWords():
             break
 
 
-    # previousCharacter = ""
+    
 
     for i in range(len(word)+1):
         for j in range(len(word)+1):
             if word[i:j] in wordDict:
                 wordDict[word[i:j]] += 1
             else:
-                # if word[i:j] != previousCharacter:
                 wordDict[word[i:j]] = 1
-                # previousCharacter = word[i:j]
 
-
-
-    # for char in word.lower():
-    #     if char in ('aeiou') and char not in vowelDict:
-    #         vowelDict[char] =  0
-    #     if char in ('aeiou') and char in vowelDict:
-    #         vowelDict[char] += 1
-    #
-    #     else:
-    #         if char not in consDict:
-    #             consDict[char] = 1
-    #         else:
-    #             consDict[char] +=1
-    # print(f"{consDict} is consdict")
-    # print(f"{vowelDict} is voweldict")
 
     return wordDict
 
