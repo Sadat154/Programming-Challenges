f595a74810b113f374fb17256123b92bdcd31021
Sadat154 <113169631+Sadat154@users.noreply.github.com>
2022-10-26 21:52:22 +0100

    Update main.py

--- Brampton Manor Academy/Programming Challenges/11 - Stock Data Mining/main.py
+++ Brampton Manor Academy/Programming Challenges/11 - Stock Data Mining/main.py
@@ -26,19 +26,20 @@ def store_in_storage(sorted_csv_file):
             x += 1
 
 
-
-
         container[x][0] += (float(i[5]) * float(i[6]))
         container[x][1] += int(i[6])
         container[x][2] = (float(container[x][0]) / float(container[x][1]))
 
+        
     list_with_dates = []
     for i in range(len(year_months)):
         list_with_dates.append((year_months[i]) + (container)[i])
 
+        
     return list_with_dates
 
-def calculate_results(sorted_csv_file, list_of_dates):
+
+def calculate_results(list_of_dates):
     top_six = sorted(list_of_dates, key = lambda x:x[4], reverse = True)
     bottom_six = sorted(list_of_dates, key = lambda x:x[4])
 
@@ -50,8 +51,10 @@ def calculate_results(sorted_csv_file, list_of_dates):
     for i in range(6):
         print(f"{i + 1}. Year: {bottom_six[i][0]} Month: {bottom_six[i][1]} with averages of: {round(bottom_six[i][4], 2)}")
 
+        
+        
 if __name__ == "__main__":
     filecontents = read_csv(Path("AAPL - AAPL.csv"))
 
     three_index_container = store_in_storage(filecontents)
-    calculate_results(filecontents, three_index_container)
+    calculate_results(three_index_container)
