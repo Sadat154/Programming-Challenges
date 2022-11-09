6cefd2bfeeb725df19f90193c8a3b8432cf98107
Sadat154 <113169631+Sadat154@users.noreply.github.com>
2022-11-09 20:59:00 +0000

    Update Calculator.java

--- Java Challenges/Firststeps/src/Calculator.java
+++ Java Challenges/Firststeps/src/Calculator.java
@@ -88,3 +88,15 @@ public class ChallengeEight {
 
     }
 }
+######################################################################################################################
+public class ChallengeNine {
+    public static void main(String[] args) {
+        String num = "153";
+        double answer_test = 0;
+        for (int i = 0; i < num.length(); i++) {
+            System.out.println(num.charAt(i));
+            answer_test += Math.pow(Integer.parseInt(String.valueOf(num.charAt(i))), 3);
+        }
+        System.out.println(answer_test);
+    }
+}
